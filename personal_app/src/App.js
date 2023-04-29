14e6581fdf37a30e566a51e6d0e092051997988a
Joshua Duplaa <joshuaduplaa@outlook.com>
2023-04-29 15:41:38 -0500

    todo objects that are togleable are now added with a button

--- personal_app/src/App.js
+++ personal_app/src/App.js
@@ -1,16 +1,28 @@
-import React, { useEffect, useState } from "react";
+//useRef hook allows us to reference elements in html or input
+import React, { useState, useRef } from "react";
 import TodoList from "./TodoList";
+
 function App() {
-  const [todos, setTodos] = useState([
-    { id: 1, name: "Todo 1", complete: false },
-  ]);
+  const [todos, setTodos] = useState([]);
+  const todoNameRef = useRef();
+
+  //argument is eventlistener
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value; //sets current value when click is listened to
+    if (name === "") return; //stops if the value is null
+    console.log(name);
+    setTodos((prevTodos) => {
+      return [...prevTodos, { id: 1, name: name, complete: false }];
+    });
+    todoNameRef.current.value = null; // makes input field blank after submitting
+  }
 
   return (
     //fragment component
     <>
       <TodoList todos={todos} />
-      <input type="text" />
-      <button>Add to do</button>
+      <input ref={todoNameRef} type="text" />
+      <button onClick={handleAddTodo}>Add to do</button>
       <button>Clear </button>
       <div>0 left to do</div>
     </>
