12f039af6e4da98085d98d08b1f9229f9f37e2ce
Joshua Duplaa <joshuaduplaa@outlook.com>
2023-04-29 14:53:59 -0500

    Prints quantity of Todos

--- personal_app/src/App.js
+++ personal_app/src/App.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TodoList from "./TodoList";
 function App() {
-  return <TodoList />;
+  const [todos, setTodos] = useState(["Todo 1", "todo 2"]);
+
+  return (
+    //fragment component
+    <>
+      <TodoList todos={todos} />
+      <input type="text" />
+      <button>Add to do</button>
+      <button>Clear </button>
+      <div>0 left to do</div>
+    </>
+  );
 }
 
 export default App;
--- personal_app/src/TodoList.js
+++ personal_app/src/TodoList.js
@@ -1,5 +1,5 @@
 import React from "react";
 
-export default function TodoList() {
-  return <div>TodoList</div>;
+export default function TodoList({ todos }) {
+  return <div>{todos.length}</div>;
 }
