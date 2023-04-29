f8c4a6567859e77ad88877e5e55b0ace0a737c56
Joshua Duplaa <joshuaduplaa@outlook.com>
2023-04-29 16:02:39 -0500

    page saves state if reloaded using the useEffect hooks

--- personal_app/src/App.js
+++ personal_app/src/App.js
@@ -1,11 +1,26 @@
 //useRef hook allows us to reference elements in html or input
-import React, { useState, useRef } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import TodoList from "./TodoList";
 import uuidv4 from "uuid/v4"; //allows us to create random IDS
+
+const LOCAL_STORAGE_KEY = "todoApp.todos";
 
 function App() {
   const [todos, setTodos] = useState([]);
   const todoNameRef = useRef();
+
+  //effect below loads our todos if page is reloaded
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (storedTodos) {
+      setTodos(storedTodos); //puts todos from local storage on page if there.
+    }
+    setTodos();
+  });
+  //anytime todos changes, it is saved to local storage using useEffect hook
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos)); // passes todos as strings into JSON file in local storage
+  }, [todos]);
 
   //argument is eventlistener
   function handleAddTodo(e) {
